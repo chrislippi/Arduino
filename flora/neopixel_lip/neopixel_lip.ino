fa3c1e766bd167f79de44cfe511fa1ca571b37b1
Chris Lippi <clippi@gmail.com>
2016-11-22 20:15:21 -0800

    updated lip neo with a bunch of colors for fun

--- flora/neopixel_lip/neopixel_lip.ino
+++ flora/neopixel_lip/neopixel_lip.ino
@@ -10,14 +10,23 @@ void setup() {
 }
 
 void loop() {
-  colorChange(strip.Color(255, 0, 0), 500); // Set Pixel to Red and wait length to 500
-  colorChange(strip.Color(0, 255, 0), 500); // Set Pixel to Red and wait length to 500
-  colorChange(strip.Color(0, 0, 255), 500); // Set Pixel to Red and wait length to 500
+  int wait=1000;
+  colorChange(strip.Color(255, 0, 0), wait); // Set to Red and wait time in ms
+  colorChange(strip.Color(255,255,255), wait); // Set to white and wait time in ms
+  colorChange(strip.Color(0, 255, 0), wait); // Set to Green and wait time in ms
+  colorChange(strip.Color(0, 0, 255), wait); // Set to Blue and wait time in ms
+  colorChange(strip.Color(165, 42, 42), wait); // Set to pink and wait time in ms
+  colorChange(strip.Color(255,140,0), wait); // Set to orange and wait time in ms
+  colorChange(strip.Color(0,255,255), wait); // Set to sea blue and wait time in ms
+  colorChange(strip.Color(255,255,0), wait); // Set to yellow and wait time in ms
+
+
 }
 
-void colorChange(uint32_t c, uint8_t wait) {
+void colorChange(uint32_t c, int wait) {
   strip.setPixelColor(0, c);
   strip.show();
   delay(wait);
+
 }
 
