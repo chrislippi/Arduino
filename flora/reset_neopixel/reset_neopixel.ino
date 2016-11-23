ab1fdedaab1205b3e8f8938bcc6cb7d2b62c9a9e
Chris Lippi <clippi@gmail.com>
2016-11-22 19:17:06 -0800

    added a bunch of neopixel playings

---
+++ flora/reset_neopixel/reset_neopixel.ino
@@ -0,0 +1,15 @@
+#include <Adafruit_NeoPixel.h>
+
+#define PIN 8
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(1, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+}
+
+void loop() {
+  // do nothing
+  
+}
